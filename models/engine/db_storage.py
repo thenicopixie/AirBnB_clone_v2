d19574e61838ce1fd0dc9b06ba3e8908ab55ef86
sumin3 <suminyu@yahoo.com>
2019-01-08 21:06:53 +0000

    add class to the cls_list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -43,7 +43,7 @@ class DBStorage:
                return a dictionary
         """
         d = {}
-        cls_list = [State, City, User]
+        cls_list = [State, City, User, Place]
         if cls:
             obj = self.__session.query(cls).all()
             key = '{}.{}'.format(obj.__class__.__name__, obj.id)
