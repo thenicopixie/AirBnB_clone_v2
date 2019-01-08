c93576851498df0d17d5c50d106968b476f65876
thenicopixie <391@holbertonschool.com>
2019-01-08 21:03:08 +0000

    fix pep8

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
+
 class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
