e93216bf15cb8e56d4776463b23f3dfcadeeecb7
thenicopixie <391@holbertonschool.com>
2019-01-08 01:48:55 +0000

    create argument for importing engines

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.dbstorage import DBStorage
-
     storage = DBStorage()
     storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
-
     storage = FileStorage()
     storage.reload()
