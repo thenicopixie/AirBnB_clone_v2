3d1cdb738078185de1623132f8c249644744df72
sumin3 <suminyu@yahoo.com>
2019-01-09 19:57:20 +0000

    add class attribute place_amenities that represent a relationship Many-To-Many

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This is the amenity class"""
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -11,3 +14,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
