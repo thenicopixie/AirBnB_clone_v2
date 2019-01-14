33e7b926ffa64eb5602ddcdd6848ffdb2f23c534
thenicopixie <391@holbertonschool.com>
2019-01-14 23:29:25 +0000

    Script that sets up the web server for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script sets up a web server for deployment
+apt-get update
+apt-get install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\nHolberton School\n  </body>\n</html>" | tee /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/^\tserver_name localhost;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
