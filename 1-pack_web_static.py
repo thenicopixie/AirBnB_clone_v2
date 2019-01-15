6562ca8e1b9926cc148fdb84d32ad747ace0628c
thenicopixie <391@holbertonschool.com>
2019-01-15 02:14:12 +0000

    generate a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fabric Script that generates a .tgz archive from the contents of
+the web_static folder using the function do_pack"""
+import datetime
+import os
+from fabric.api import local
+now = datetime.datetime.now()
+
+
+def do_pack():
+    """Generates a .tgz archive"""
+    local("mkdir -p versions")
+    current_time = now.strftime("%Y%m%d%H%M%S")
+    tgz_file = 'web_static_' + current_time + '.tgz'
+    local("tar -cvzf {} ./web_static".format(tgz_file))
+    local("mv {} versions".format(tgz_file))
+    fpath = "readlink -f {}".format(tgz_file)
+    return fpath
