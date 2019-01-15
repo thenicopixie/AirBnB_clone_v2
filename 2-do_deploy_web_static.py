69dfa8d700f27c3805305c3ecf9e879fb6dd9380
thenicopixie <391@holbertonschool.com>
2019-01-15 03:54:25 +0000

    distributes an archive to web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""calls do_deploy to distribute the archives to the web server"""
+from fabric.api import *
+import os
+import datetime
+now = datetime.datetime.now()
+
+
+env.hosts = ['35.227.97.247', '35.196.203.239']
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Generates a .tgz archive"""
+    try:
+        local("mkdir -p versions")
+        current_time = now.strftime("%Y%m%d%H%M%S")
+        tgz_file = 'web_static_' + current_time + '.tgz'
+        local("tar -cvzf {} ./web_static".format(tgz_file))
+        local("mv {} versions".format(tgz_file))
+        fpath = "version/web_static{}.tgz".format(current_time)
+        return fpath
+    except Exception:
+        return None
+
+def do_deploy(archive_path):
+    """Distributes an archive to te web server"""
+    result = run("ls -l {}".format(archive_path))
+    if result.failed:
+        return False
+
+    rel = "/data/web_static/releases/"
+    fpath = os.path.basename("{}".format(archive_path))
+
+    try:
+        """upload archive to the /tmp directory"""
+        put(archive_path, "/tmp/")
+
+        """uncompress the archive to the folder /data/web_static/releases/..."""
+        run("mkdir -p {}{}".format(rel, fpath))
+        run("tar -xzf /tmp/{} -C {}{}".format(fpath, rel, fpath))
+
+        """delete the archive from the web server"""
+        run("rm /tmp/{}".format(fpath))
+
+        run("mv {}{}web_static/* {}{}/".format(rel, fpath, rel, fpath))
+        """delete the symbolic link from web server"""
+        run("rm -rf {}{}/web_static".format(rel, fpath))
+        run("rm -rf /data/web_static/current")
+
+        """create a symlink between files"""
+        run("ln -s {}{} /data/web_static/current".format(rel, fname))
+        return True
+    except Exception:
+        return False
+
