5f50d139ddf6eb3ed9c7495ac48e1b20ff3f48dc
thenicopixie <391@holbertonschool.com>
2019-01-29 22:19:45 +0000

    print states and cities, or state based on id

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(stuff):
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    states = list(storage.all("State").values())
+    states.sort(key=lambda i: i.name)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states/', strict_slashes=False)
+def states_id(id=None):
+    states = list(storage.all("State").values())
+    cities = list(storage.all("City").values())
+    cities.sort(key=lambda i: i.name)
+    return render_template('9-states.html',
+                           states=states, cities=cities, id=id)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
